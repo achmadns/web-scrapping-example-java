3bac9854b8719a37b8850c800bdc285207610a38
Achmad Nasirudin Sandi <achmad.dev@gmail.com>
2021-10-28 00:13:14 +0700

    Selenium experiment.

--- src/test/java/com/example/scrapping/ScrappingPlayGround.java
+++ src/test/java/com/example/scrapping/ScrappingPlayGround.java
@@ -1,18 +1,17 @@
 package com.example.scrapping;
 
-import org.apache.commons.lang3.StringUtils;
+//import org.apache.commons.lang3.StringUtils;
+
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.regex.Pattern;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -24,39 +23,26 @@ public class ScrappingPlayGround {
         assertThat(Files.readAllLines(Paths.get("newline.csv"))).hasSize(1);
     }
 
-    @Test
-    public void extractShopNameFromBodyShouldSuccess() {
-        final String body = "\"shopID\":\"342169\",\"shopName\":\"Gudang-HP\",\"minOrder\":1,\"maxOrder\":14,\"weight\":500";
-        final Pattern pattern = Pattern.compile("\"shopName\":\"(.*?)\",\"minOrder\"");
-        final String shopName = StringUtils.substringBetween(body, "\"shopName\":\"", "\",\"minOrder\"");
-//        final String shopName = pattern.matcher(body).results().findFirst().get().group(0);
-        assertThat(shopName).isEqualTo("Gudang-HP");
-    }
 
-
-    @ParameterizedTest
-    @CsvFileSource(resources = "/url-cases.csv", numLinesToSkip = 1)
-    public void extractProductLinkFromClickableLink(String expectedLink, String completeLink) {
-        assertThat(WebScrappingExample.extractAndDecodeProductLink(completeLink)).isEqualTo(expectedLink);
-    }
-
-//    @Test
-//    public void openWebPage() throws InterruptedException {
-//        System.setProperty("webdriver.chrome.driver", "/home/achmad/bin/chromedriver");
-//        final WebDriver driver = new ChromeDriver();
-//        driver.get("http://www.google.com/");
-//
-//        Thread.sleep(5000);  // Let the user actually see something!
-//
-//        WebElement searchBox = driver.findElement(By.name("q"));
-//
-//        searchBox.sendKeys("ChromeDriver");
-//
-//        searchBox.submit();
-//
-//        Thread.sleep(5000);  // Let the user actually see something!
-//
-//        driver.quit();
+//    @ParameterizedTest
+//    @CsvFileSource(resources = "/url-cases.csv", numLinesToSkip = 1)
+//    public void extractProductLinkFromClickableLink(String expectedLink, String completeLink) {
+//        assertThat(WebScrappingExample.extractAndDecodeProductLink(completeLink)).isEqualTo(expectedLink);
 //    }
+
+    @Test
+    public void openWebPage() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "/home/achmad/bin/chromedriver");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setPageLoadStrategy(PageLoadStrategy.EAGER);
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+
+        driver.get("https://www.tokopedia.com/p/handphone-tablet/handphone?page=1");
+        //This will scroll the web page till end.
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+        driver.quit();
+    }
 }
 
